f49c74ee2b7b601b3ffd50a5be4e4206f2cba423
Jonathan Lam <jdlam420@gmail.com>
2016-03-19 19:57:43 -0400

    adds front end validation for home page

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,4 +1,7 @@
-$(document).ready(bindSubmit);
+$(document).ready(function(){
+  formValidation();
+  bindSubmit();
+});
 
 function bindSubmit() {
   $('#searchMovie').submit(function(){
@@ -11,4 +14,14 @@ function checkVal(id) {
   if ( $(id).val()=='' ) {
     $(id).prop('disabled', true)
   }
+}
+
+function formValidation() {
+  $('#title').on('keyup', function(){
+    if ($('#title').val().length > 0) {
+      $('input[type=submit]').prop('disabled',false)
+    } else {
+      $('input[type=submit]').prop('disabled',true)
+    }
+  });
 }
